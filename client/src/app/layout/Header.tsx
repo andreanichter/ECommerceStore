ab773542b037162c74815ad154f42c6c256d4716
andreanichter <andreamnichter@gmail.com>
2024-05-29 08:25:51 +0200

    Set up react router

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,17 +1,35 @@
-import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
+import { AppBar, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
+
+const midLinks = [
+  { title: "catalog", path: "/catalog" },
+  { title: "about", path: "/about" },
+  { title: "contact", path: "/contact" },
+];
+
+const rightLinks = [
+  { title: "login", path: "/login" },
+  { title: "register", path: "/register" },
+];
 
 interface Props {
-    darkMode: boolean;
-    handleThemeChange: () => void;
+  darkMode: boolean;
+  handleThemeChange: () => void;
 }
 
-export default function Header({darkMode, handleThemeChange}: Props) {
-    return(
-        <AppBar position="static" sx={{mb: 4}}>
-            <Toolbar>
-                <Typography variant="h6">RE-STORE</Typography>
-                <Switch checked={darkMode} onChange={handleThemeChange}/>
-            </Toolbar>
-        </AppBar>
-    )
+export default function Header({ darkMode, handleThemeChange }: Props) {
+  return (
+    <AppBar position="static" sx={{ mb: 4 }}>
+      <Toolbar>
+        <Typography variant="h6">RE-STORE</Typography>
+        <Switch checked={darkMode} onChange={handleThemeChange} />
+        <List sx={{display: "flex"}}>
+            {midLinks.map(({title, path}) => (
+                <ListItem>
+                    {title.toUpperCase()}
+                </ListItem>
+            ))}
+        </List>
+      </Toolbar>
+    </AppBar>
+  );
 }
