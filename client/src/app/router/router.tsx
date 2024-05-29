8ebe6645d6b531264881ecce14accbb7415272dd
andreanichter <andreamnichter@gmail.com>
2024-05-29 21:33:50 +0200

    Add imports

--- client/src/app/router/router.tsx
+++ client/src/app/router/router.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../layout/App";
+import HomePage from "../../features/home/HomePage";
 
 export const router = createBrowserRouter([
   {
