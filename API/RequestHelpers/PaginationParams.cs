ece69f938be459b9aac8c93a3f46dff3003a9044
andreanichter <andreamnichter@gmail.com>
2024-06-14 13:30:23 +0200

    Add pagination functionality to backend

--- API/RequestHelpers/PaginationParams.cs
+++ API/RequestHelpers/PaginationParams.cs
@@ -3,7 +3,7 @@ namespace API.RequestHelpers
     public class PaginationParams
     {
         private const int MaxPageSize = 50;
-        public int PageNUmber { get; set; } = 1;
+        public int PageNumber { get; set; } = 1;
         private int _pageSize = 6;
         public int PageSize
         {
