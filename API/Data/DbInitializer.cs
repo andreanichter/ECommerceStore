edbee8e676e435378035d096d5740938556a1f87
andreanichter <andreamnichter@gmail.com>
2024-07-04 09:34:30 +0200

    Set up Identity for user roles

--- API/Data/DbInitializer.cs
+++ API/Data/DbInitializer.cs
@@ -1,11 +1,25 @@
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
 
 namespace API.Data
 {
     public static class DbInitializer
     {
-        public static void Initialize(StoreContext context)
+        public static async Task Initialize(StoreContext context, UserManager<User> userManager)
         {
+            if (!userManager.Users.Any())
+            {
+                var user = new User { UserName = "testUser", Email = "test@test.com" };
+
+                await userManager.CreateAsync(user, "Pa$$w0rd");
+                await userManager.AddToRoleAsync(user, "Member");
+
+                var admin = new User { UserName = "admin", Email = "admin@admin.com" };
+
+                await userManager.CreateAsync(admin, "Pa$$w0rd");
+                await userManager.AddToRolesAsync(admin, ["Member", "Admin"]);
+            }
+
             if (context.Products.Any())
                 return;
 
