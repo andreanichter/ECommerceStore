918100faa160dba4b595eb1466c356889e36619e
andreanichter <andreamnichter@gmail.com>
2024-07-04 18:34:06 +0200

    Add form validators to login

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -16,11 +16,17 @@ export default function Login() {
   const {
     register,
     handleSubmit,
-    formState: { isSubmitting },
-  } = useForm();
+    formState: { isSubmitting, errors, isValid },
+  } = useForm({
+    mode: "onTouched"
+  });
 
   async function submitForm(data: FieldValues) {
-    await agent.Account.login(data);
+    try {
+      await agent.Account.login(data);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
@@ -52,17 +58,22 @@ export default function Login() {
           fullWidth
           label="Username"
           autoFocus
-          {...register("username")}
+          {...register("username", { required: "Username is required" })}
+          error={!!errors.username}
+          helperText={errors?.username?.message as string}
         />
         <TextField
           margin="normal"
           fullWidth
           label="Password"
           type="password"
-          {...register("password")}
+          {...register("password", { required: "Password is required" })}
+          error={!!errors.password}
+          helperText={errors?.password?.message as string}
         />
         <LoadingButton
           loading={isSubmitting}
+          disabled = {!isValid}
           type="submit"
           fullWidth
           variant="contained"
