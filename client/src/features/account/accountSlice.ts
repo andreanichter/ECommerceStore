a0cac464201522bb29b5a397670048f64008d7e5
andreanichter <andreamnichter@gmail.com>
2024-07-05 11:46:29 +0200

    Handle basket transfer on the client side

--- client/src/features/account/accountSlice.ts
+++ client/src/features/account/accountSlice.ts
@@ -4,6 +4,7 @@ import { FieldValues } from "react-hook-form";
 import agent from "../../app/api/agent";
 import { router } from "../../app/router/Routes";
 import { toast } from "react-toastify";
+import { setBasket } from "../basket/basketSlice";
 
 interface AccountState {
   user: User | null;
@@ -17,8 +18,11 @@ export const signInUser = createAsyncThunk<User, FieldValues>(
   "account/signInUser",
   async (data, thunkAPI) => {
     try {
-      const user = await agent.Account.login(data);
+      const userDto = await agent.Account.login(data);
+      const { basket, ...user } = userDto;
+      if (basket) thunkAPI.dispatch(setBasket(basket));
       localStorage.setItem("user", JSON.stringify(user));
+
       return user;
     } catch (error: any) {
       return thunkAPI.rejectWithValue({ error: error.data });
@@ -29,10 +33,13 @@ export const signInUser = createAsyncThunk<User, FieldValues>(
 export const fetchCurrentUser = createAsyncThunk<User>(
   "account/fetchCurrentUser",
   async (_, thunkAPI) => {
-    thunkAPI.dispatch(setUser(JSON.parse(localStorage.getItem("user")!)))
+    thunkAPI.dispatch(setUser(JSON.parse(localStorage.getItem("user")!)));
     try {
-      const user = await agent.Account.currentUser();
+      const userDto = await agent.Account.currentUser();
+      const { basket, ...user } = userDto;
+      if (basket) thunkAPI.dispatch(setBasket(basket));
       localStorage.setItem("user", JSON.stringify(user));
+
       return user;
     } catch (error: any) {
       return thunkAPI.rejectWithValue({ error: error.data });
@@ -56,27 +63,24 @@ export const accountSlice = createSlice({
     },
     setUser: (state, action) => {
       state.user = action.payload;
-    }
+    },
   },
   extraReducers: (builder) => {
     builder.addCase(fetchCurrentUser.rejected, (state) => {
       state.user = null;
       localStorage.removeItem("user");
-      toast.error("Session expired - please log in again!")
-      router.navigate("/")
-    })
+      toast.error("Session expired - please log in again!");
+      router.navigate("/");
+    });
     builder.addMatcher(
       isAnyOf(signInUser.fulfilled, fetchCurrentUser.fulfilled),
       (state, action) => {
         state.user = action.payload;
       }
     );
-    builder.addMatcher(
-      isAnyOf(signInUser.rejected),
-      (_state, action) => {
-        console.log(action.payload);
-      }
-    );
+    builder.addMatcher(isAnyOf(signInUser.rejected), (_state, action) => {
+      throw(action.payload);
+    });
   },
 });
 
