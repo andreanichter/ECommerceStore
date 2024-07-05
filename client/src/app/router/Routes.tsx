16bb2d210915ef0da18ba67c466d9ab773007612
andreanichter <andreamnichter@gmail.com>
2024-07-05 11:53:50 +0200

    Handle auth requirements for routes on the client side

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -11,12 +11,17 @@ import BasketPage from "../../features/basket/BasketPage";
 import CheckoutPage from "../../features/checkout/CheckoutPage";
 import Register from "../../features/account/Register";
 import Login from "../../features/account/Login";
+import RequireAuth from "./RequireAuth";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
+      {
+        element: <RequireAuth />,
+        children: [{ path: "checkout", element: <CheckoutPage /> }],
+      },
       { path: "", element: <HomePage /> },
       { path: "catalog", element: <Catalog /> },
       { path: "catalog/:id", element: <ProductDetails /> },
@@ -25,8 +30,8 @@ export const router = createBrowserRouter([
       { path: "server-error", element: <ServerError /> },
       { path: "not-found", element: <NotFound /> },
       { path: "basket", element: <BasketPage /> },
-      { path: "checkout", element: <CheckoutPage /> },
-      { path: "login", element: <Login/> },
+
+      { path: "login", element: <Login /> },
       { path: "register", element: <Register /> },
       { path: "*", element: <Navigate replace to="/not-found" /> },
     ],
