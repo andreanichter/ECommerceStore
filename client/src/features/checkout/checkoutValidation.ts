291cf5678de28849ac125d18b76d2d922257d244
andreanichter <andreamnichter@gmail.com>
2024-07-10 14:09:00 +0200

    Add checkout validation schema

---
+++ client/src/features/checkout/checkoutValidation.ts
@@ -0,0 +1,12 @@
+import * as yup from "yup";
+
+export const validationSchema = yup.object({
+  fullName: yup.string().required("Full name is required"),
+
+  address1: yup.string().required("Address is required"),
+  address2: yup.string().required("Address is required"),
+  city: yup.string().required("City name is required"),
+  state: yup.string().required("State name is required"),
+  zip: yup.string().required("Postal code is required"),
+  country: yup.string().required("Country name is required"),
+});
