6518d80a5e5ebee1989eb776060cda29a86becaa
andreanichter <andreamnichter@gmail.com>
2024-07-11 12:56:52 +0200

    Add more styling to HomePage

--- client/src/features/home/HomePage.tsx
+++ client/src/features/home/HomePage.tsx
@@ -1,9 +1,13 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Testimonials from "./Testimonials";
+import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
+  const navigate = useNavigate();
+
   const settings = {
     dots: true,
     infinite: true,
@@ -37,15 +41,30 @@ export default function HomePage() {
           />
         </div>
       </Slider>
-      <Box display="flex" justifyContent="center" mt={4}>
-        <Typography variant="h1" component="h1" gutterBottom>
-          Welcome to the store!
+      <Box display="flex" justifyContent="center" mt={8}>
+        <Typography variant="h2" component="h2" gutterBottom>
+          Welcome to React Realm!
         </Typography>
       </Box>
       <Box display="flex" justifyContent="center" mb={4}>
         <Typography variant="h5" component="p" align="center" maxWidth="md">
-          Discover the best deals on the latest products. We are committed to providing the best shopping experience. Our store offers a wide variety of products at competitive prices. Our mission is to ensure customer satisfaction with every purchase.
+          Discover the best deals on the latest products. We are committed to
+          providing the best shopping experience. Our store offers a wide
+          variety of products at competitive prices. Our mission is to ensure
+          customer satisfaction with every purchase.
         </Typography>
+      </Box>
+      <Box display="flex" justifyContent="center" mb={1}>
+        <Button
+          onClick={() => navigate("/catalog")}
+          variant="contained"
+          color="primary"
+        >
+          Shop Now
+        </Button>
+      </Box>
+      <Box>
+        <Testimonials />
       </Box>
     </>
   );
