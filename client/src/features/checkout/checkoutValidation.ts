d16e73bd2fb65a2eb0d57b00ff98acf35ffdfe15
andreanichter <andreamnichter@gmail.com>
2024-07-11 10:21:23 +0200

    Add nameOnCard validation to validationSchema

--- client/src/features/checkout/checkoutValidation.ts
+++ client/src/features/checkout/checkoutValidation.ts
@@ -1,12 +1,18 @@
 import * as yup from "yup";
 
-export const validationSchema = yup.object({
-  fullName: yup.string().required("Full name is required"),
+export const validationSchema = [
+  yup.object({
+    fullName: yup.string().required("Full name is required"),
 
-  address1: yup.string().required("Address is required"),
-  address2: yup.string().required("Address is required"),
-  city: yup.string().required("City name is required"),
-  state: yup.string().required("State name is required"),
-  zip: yup.string().required("Postal code is required"),
-  country: yup.string().required("Country name is required"),
-});
+    address1: yup.string().required("Address is required"),
+    address2: yup.string().required("Address is required"),
+    city: yup.string().required("City name is required"),
+    state: yup.string().required("State name is required"),
+    zip: yup.string().required("Postal code is required"),
+    country: yup.string().required("Country name is required"),
+  }),
+  yup.object(),
+  yup.object({
+    nameOnCard: yup.string().required("Name on card is required"),
+  }),
+];
